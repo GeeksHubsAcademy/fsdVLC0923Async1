8ec2357f60756c9cd5dba386d5785f286731c121
Dave86dev <davidochando1986@gmail.com>
2023-09-29 13:38:28 +0200

    Basic API axios get fetching data

---
+++ main.js
@@ -0,0 +1,23 @@
+
+const axios = require("axios");
+
+console.log("YO SOY EL PRIMERO SALIR");
+
+const bringLocations = async () => {
+
+    console.log("AHORA LLAMO A LA API");
+
+    let resultado = await axios.get("https://rickandmortyapi.com/api/location?page=2");
+    
+    console.log(resultado.data.results);
+
+}
+
+bringLocations();
+
+setTimeout(()=>{
+    console.log("NECESITO ALGO PARA OLVIDAR");
+
+},2000)
+
+
